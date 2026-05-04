3ad0e1fef17cb7559ece5f506335642b4ac39cf2
Vashu <vasuchr09@gmail.com>
2026-05-04 09:24:36 +0530

    Security: Remove exposed secrets and move to environment variables

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -6,13 +6,13 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1d-VrrjWl_cy1xWBqKk80T9rfBo9aYFw",
-  authDomain: "harshita-ride-pro.firebaseapp.com",
-  projectId: "harshita-ride-pro",
-  storageBucket: "harshita-ride-pro.firebasestorage.app",
-  messagingSenderId: "720508259433",
-  appId: "1:720508259433:web:6c789b8e773020eab7a6f8",
-  measurementId: "G-W9DQV9VC5Y"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
